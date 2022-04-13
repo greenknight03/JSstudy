5da40c3dea6b16a63f4f0cef10ef8f0de45e6b17
Liz <92960723+greenknight03@users.noreply.github.com>
2022-04-13 23:34:33 +0900

    📖 Logical Operator

--- Fundemental-Part-1/script.js
+++ Fundemental-Part-1/script.js
@@ -351,3 +351,29 @@ if (favourite === 23) { // 22 === 23 -> FALSE
   console.log('Number is not 23 or 7 or 9')
 }sole.log('Number is not 23 or 7')
  */
+
+////////////////////////////////////////////////
+// 23. Logical Operator
+/* 
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log('Sarah is able to drive!');
+// } else {
+//   console.log('Someone else should drive...');
+// }
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log('Sarah is able to drive!');
+} else {
+  console.log('Someone else should drive...');
+}
+ */
