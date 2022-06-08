cf5fceb3b5b17f0007ca309c2b2a759c6de743d0
Liz <92960723+greenknight03@users.noreply.github.com>
2022-06-09 00:16:10 +0900

    ✨ Add :  Deactivate the button when the game is over & Get rid of dice

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -21,6 +21,7 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0; // let -> Keep updating
 let activePlayer = 0;
+let playing = true;
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -32,47 +33,52 @@ const switchPlayer = function () {
 
 // Rollig dice funtionality
 btnRoll.addEventListener('click', function () {
-  // 1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
-  // 2. Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1
-  if (dice !== 1) {
-    // Add dice to current score
-    // currentScore = currentScore + dice;
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to next player
-    switchPlayer();
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to current score
+      // currentScore = currentScore + dice;
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  console.log(scores[activePlayer]);
+  if (playing) {
+    console.log(scores[activePlayer]);
 
-  // 1. Add current score to active player's score
-  scores[activePlayer] += currentScore;
-  // scores[1] = scores[1] + currentScore
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
-  // 2. Check if player's score is >= 100
-  if (scores[activePlayer] >= 10) {
-    // Finish the game
-    document
-      .querySelector(`player--${activePlayer}`)
-      .classList.add('player--winner');
-    document
-      .querySelector(`player--${activePlayer}`)
-      .classList.remove('player--active');
-  } else {
-    // Switch to the next player
-    switchPlayer();
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 20) {
+      // Finish the game
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
   }
 });
