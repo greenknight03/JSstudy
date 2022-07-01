cd6a72e3e5828953cfb55ba5e0dfba2d568c23b5
Liz <92960723+greenknight03@users.noreply.github.com>
2022-07-01 13:58:34 +0900

    📖 The this Keyword in Practice

--- JS-Behind-The-Scene/script.js
+++ JS-Behind-The-Scene/script.js
@@ -39,7 +39,7 @@ const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age); // Uncaught ReferenceError : age is not defined
 // printAge(); // Uncaught ReferenceError : printge is not defined
-*/
+
 
 /////////////////////////////////////////////////////////////////////
 // Hosting and TDZ in Practice
@@ -87,3 +87,41 @@ console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
 console.log();
+*/
+
+///////////////////////////////////////
+// The this Keyword in Practice
+// console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  // console.log(this); // undefined
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  // console.log(this); // Window {window: Window, self: Window, document...
+  // simply the this keyword the parent's scope
+};
+calcAgeArrow(1980);
+
+// this keyword inside of a method
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
